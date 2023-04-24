2c0a3d952b9e0ca93feb7c5f69be939841e413ed
lekdup <lekdup@live.fr>
2023-04-24 22:25:15 +0200

    initial commit

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function longestPalindrome (s) {
+
     // if s is declared less than a character, than it doesn't have a substring so we can return empty
     if (s.length < 1) return "";
+
+    // these 2 variables are for substr() parameter, 1st argument is starting index of the substring and 2nd optional argument is the length of the substring.
     let maxSubStart = 0;
     let maxSubLength = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        let lengthCenteredAtChar = expandAroundCenter() //expandFromMiddle()
+    }
 
     return s.substr(maxSubStart, maxSubLength);
 }
