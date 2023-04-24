bdf217c1b18796adc4b513067ab43d4d00f42e03
lekdup <lekdup@live.fr>
2023-04-24 22:23:49 +0200

    initial commit

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function longestPalindrome (s) {
     // if s is declared less than a character, than it doesn't have a substring so we can return empty
-    if (s.length < 1);
-    return "";
+    if (s.length < 1) return "";
+    let maxSubStart = 0;
+    let maxSubLength = 0;
+
+    return s.substr(maxSubStart, maxSubLength);
 }
+let s = "babad";
+console.log(longestPalindrome(s));
