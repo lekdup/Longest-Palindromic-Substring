efa8cf86594e9048688e79e3f007d30b5e32afed
lekdup <lekdup@live.fr>
2023-04-26 20:27:12 +0200

    third solution

--- script.js
+++ script.js
@@ -1,17 +1,96 @@
-function longestPalindrome (s) {
+// function longestPalindrome (s) {
 
-    // if s is declared less than a character, than it doesn't have a substring so we can return empty
-    if (s.length < 1) return "";
+//     // if s is declared less than a character, than it doesn't have a substring so we can return empty
+//     if (s.length < 1) return "";
 
-    // these 2 variables are for substr() parameter, 1st argument is starting index of the substring and 2nd optional argument is the length of the substring.
-    let maxSubStart = 0;
-    let maxSubLength = 0;
+//     // these 2 variables are for substr() parameter, 1st argument is starting index of the substring and 2nd optional argument is the length of the substring.
+//     let maxSubStart = 0;
+//     let maxSubLength = 0;
 
-    for (let i = 0; i < s.length; i++) {
-        let lengthCenteredAtChar = expandAroundCenter() //expandFromMiddle()
+//     for (let i = 0; i < s.length; i++) {
+//         let lengthCenteredAtChar = expandAroundCenter(s, i, i); //expandFromMiddle()
+//         let lengthCenteredAtSpace = expandAroundCenter(s, i, i + 1);
+//         let longestSubAtChar = Math.max(lengthCenteredAtChar, lengthCenteredAtSpace)
+//         if (longestSubAtChar > maxSubLength) {
+//             maxSubLength = longestSubAtChar;
+//             maxSubStart = i - Math.floor((maxSubLength  - 1) / 2 )
+//         }
+//     }
+
+//     return s.substr(maxSubStart, maxSubLength);
+// }
+
+// function expandAroundCenter(s, left, right) {
+//     while (left >= 0 && right < s.length && s[left] === s[right]) {
+//         left--;
+//         right++
+//     }
+//     return right - left - 1;
+// }
+// let s = "cbbd";
+// console.log(longestPalindrome(s));
+
+
+
+// var longestPalindrome = function(s) {
+//     let max = [0, 1];
+    
+//     for ( let i = 0; i < s.length; i++ ) {
+//         let even = getPalindrome(i - 1, i, s);
+//         let odd = getPalindrome(i - 1, i + 1, s);
+        
+//         let curMax = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
+        
+//         max = max[1] - max[0] > curMax [1] - curMax[0] ? max : curMax;
+//     }
+    
+//     return s.slice(max[0], max[1]);
+// }
+// let getPalindrome = (left, right, s) => {
+//     while (left >= 0 && right < s.length) {
+//         if(s[left] !== s[right]) break;
+//         left--;
+//         right++;
+//     }
+//     return [left + 1, right]
+// }
+
+// console.log(longestPalindrome("a"));
+
+
+
+var longestPalindrome = function (str) {
+    if( str.length < 1 || str === null) return "";
+
+    let longestSubstring = "";
+
+    for (let i = 0; i < str.length; i++) {
+        let oddPalindrome = expandFromCenter(str, i, i);
+        let evenPalindrome = expandFromCenter(str, i-1, i);
+
+        if( oddPalindrome.length > longestSubstring.length) {
+            longestSubstring = oddPalindrome;
+        }
+        if(evenPalindrome.length > longestSubstring.length){
+            longestSubstring = evenPalindrome;
+        }
     }
-
-    return s.substr(maxSubStart, maxSubLength);
+    return longestSubstring;
 }
+
+function expandFromCenter(str, left, right) {
+    let i = 0;
+    while(str[left - i] && str[left-i] === str[right + i]){
+        i++;
+    }
+    i--;
+    
+
+    return str.slice(left - i, right + i + 1);
+}
+
+console.log(longestPalindrome("cbbd"));
+
 let s = "babad";
-console.log(longestPalindrome(s));
+
+console.log(s.slice(1 - 1, 3 + 1 + 1));
